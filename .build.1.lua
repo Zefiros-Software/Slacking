edd076cb927e175f28dd250d53bd519a57554b1b
PaulVisscher <paul.pev@hotmail.com>
2016-09-16 09:20:01 +0200

    Update .build.1.lua
    kind StaticLib

--- .build.1.lua
+++ .build.1.lua
@@ -1,16 +1,7 @@
 
 project "Slacking"
 
-    filter "platforms:Windows"
-        kind "Utility"
-
-    filter "platforms:Unix"
-        kind "StaticLib"
-
-    filter "platforms:Mac"
-        kind "StaticLib"
-
-    filter {}
+    kind "StaticLib"
 
     zpm.uses {
         "Zefiros-Software/cURL"
