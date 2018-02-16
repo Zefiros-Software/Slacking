2c59f63eba93eb29b488293452aa970eff622293
PaulVisscher <paul.pev@hotmail.com>
2018-02-16 19:27:49 +0100

    Update to zpm 2

--- script/appveyor-build.bat
+++
@@ -1,7 +0,0 @@
-cd test/
-zpm install-package --allow-install --allow-module  || exit /b 1
-zpm vs2015 --allow-install || exit /b 1
-
-msbuild Slacking.sln /property:Configuration=Test /property:Platform=x64 || exit /b 1
-
-.\bin\Test\Slacking.exe || exit /b 1
